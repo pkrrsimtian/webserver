3189066131ce3a4ff1557b1ad1a4d17d2430286d
PKumar <pkrrsimtian@gmail.com>
2020-05-10 00:21:14 +0530

    initial

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>HTML attributes example</title>
+</head>
+<body>
+   <h1>This is normal heading</h1>
+   <h1 style="font-size:10px; color:#ff0000">
+        This is heading with attribute.</h1>
+   <p style="background-color:#ffffcc">
+     As you see, the background of the paragraph has changed.
+     You can do a lot more with style attribute.
+   </p>
+</body>
+</html>
